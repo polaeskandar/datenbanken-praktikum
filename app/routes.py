37676316312b981c6b4d90cb74291e9930dcf193
Pola Eskandar <pola.eskandar@codingstreamer.com>
2024-11-30 14:55:12 +0100

    Finished navbar

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.components.navbar_component import navbar_component
 routes_blueprint = Blueprint("routes", __name__)
 
 
-@routes_blueprint.route("/")
+@routes_blueprint.route("/", methods=["GET", "POST"])
 def index():
     components = {
         "header": [
@@ -15,4 +15,8 @@ def index():
         "footer": [],
     }
 
-    return render_template("layout.html", components=components)
+    return render_template(
+        "layout.html",
+        components=components,
+        page_title="Home",
+    )
