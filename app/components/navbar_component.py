c00bee428c236a1f1eb27b900c5629131dc9ee6d
Pola Eskandar <pola.eskandar@codingstreamer.com>
2024-11-30 14:55:12 +0100

    Finished navbar

--- app/components/navbar_component.py
+++ app/components/navbar_component.py
@@ -1,10 +1,27 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, flash
+from flask_wtf import FlaskForm
+
+from app.form.component.navbar.RestaurantsFilterForm import RestaurantsFilterForm
 
 
 def navbar_component():
+    restaurants_filter_form = RestaurantsFilterForm(request.args)
+
+    if not restaurants_filter_form.validate():
+        for error in restaurants_filter_form.errors.values():
+            flash(error[0], category="danger")
+
     attributes = {
         "logo": url_for("static", filename="images/logo.png"),
         "index_route": url_for("routes.index"),
+        "restaurants_filter_form": restaurants_filter_form,
     }
 
     return render_template("components/navbar.html", attributes=attributes)
+
+
+def validate_forms(forms: list[FlaskForm]):
+    for form in forms:
+        if not form.validate():
+            for error in form.errors.values():
+                flash(error[0], category="danger")
