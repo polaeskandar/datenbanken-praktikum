a9388cf806d02fae870450ba6f81994b2f5f23a1
Pola Eskandar <pola.eskandar@codingstreamer.com>
2024-12-08 21:36:27 +0100

    Add opening hours component

--- app/components/admin/aside_menu_component.py
+++ app/components/admin/aside_menu_component.py
@@ -1,5 +1,39 @@
-from flask import render_template
+from flask import render_template, url_for
 
 
 def aside_menu_component():
-    return render_template("components/admin/aside_menu.html")
+    attributes = {
+        "links": get_links(),
+    }
+
+    return render_template("components/admin/aside_menu.html", attributes=attributes)
+
+
+def get_links() -> list[dict[str, str]]:
+    return [
+        {
+            "icon": "fa-solid fa-receipt me-2",
+            "text": "Orders",
+            "href": "#",
+        },
+        {
+            "icon": "fa-solid fa-utensils me-2",
+            "text": "Menu",
+            "href": "#",
+        },
+        {
+            "icon": "fa-solid fa-clock me-2",
+            "text": "Opening Hours",
+            "href": url_for("admin.opening_hours"),
+        },
+        {
+            "icon": "fa-solid fa-truck me-2",
+            "text": "Delivery Radius",
+            "href": "#",
+        },
+        {
+            "icon": "fa-solid fa-gears me-2",
+            "text": "Settings",
+            "href": "#",
+        },
+    ]
--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,14 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 
 from app.components.admin.aside_menu_component import aside_menu_component
+from app.components.admin.opening_times_component import opening_times_component
 from app.components.footer_component import footer_component
 from app.components.navbar_component import navbar_component
 from app.routes import render_page
 
 admin_routes = Blueprint("admin", __name__)
 
+
 @admin_routes.route("/")
-def index():
+def index() -> Response:
     components = {
         "header": [
             navbar_component(),
@@ -23,3 +25,23 @@ def index():
     }
 
     return render_page("admin.html", "Admin", components)
+
+
+@admin_routes.route("/opening-hours", methods=["GET", "POST"])
+def opening_hours() -> Response:
+    components = {
+        "header": [
+            navbar_component(),
+        ],
+        "aside": [
+            aside_menu_component(),
+        ],
+        "main": [
+            opening_times_component(),
+        ],
+        "footer": [
+            footer_component(),
+        ],
+    }
+
+    return render_page("admin.html", "Opening Hours", components)
