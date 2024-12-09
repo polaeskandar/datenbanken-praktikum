53d0a393d5a503c38f59689d458b5c5ef51b21d2
thejawadd <mohamadali.jawad@icloud.com>
2024-12-09 18:54:35 +0200

    order history

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from flask import render_template, Blueprint
 
 from app.components.navbar_component import navbar_component
 from app.components.footer_component import footer_component
+from app.components.restaurant_card_component import restaurant_card_component
+from app.components.best_bets_component import best_bets_component
+from app.components.restaurant_rating_component import restaurant_rating_component
+from app.components.order_history_component import order_history_component
 
 routes_blueprint = Blueprint("routes", __name__)
 
@@ -12,7 +16,11 @@ def index():
         "header": [
             navbar_component(),
         ],
-        "main": [],
+        "main": [
+            best_bets_component(),
+            restaurant_card_component(),
+            restaurant_rating_component()
+        ],
         "footer": [
             footer_component(),
         ],
