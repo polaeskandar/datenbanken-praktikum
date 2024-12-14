bc1e0beaa2373c3e9f57072d790161c51bbbcd69
Pola Eskandar <pola.eskandar@codingstreamer.com>
2024-12-14 02:52:00 +0100

    Small aside adjust

--- app/components/admin/aside_menu_component.py
+++ app/components/admin/aside_menu_component.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 
 
 def aside_menu_component():
@@ -12,28 +12,33 @@ def aside_menu_component():
 def get_links() -> list[dict[str, str]]:
     return [
         {
-            "icon": "fa-solid fa-receipt me-2",
-            "text": "Orders",
-            "href": "#",
-        },
-        {
             "icon": "fa-solid fa-utensils me-2",
             "text": "Menu",
             "href": "#",
+            "is_active": False,
+        },
+        {
+            "icon": "fa-solid fa-receipt me-2",
+            "text": "Orders",
+            "href": "#",
+            "is_active": False,
         },
         {
             "icon": "fa-solid fa-clock me-2",
             "text": "Opening Hours",
             "href": url_for("admin.opening_hours"),
+            "is_active": request.path == url_for('admin.opening_hours'),
         },
         {
             "icon": "fa-solid fa-truck me-2",
             "text": "Delivery Radius",
             "href": url_for("admin.delivery_radius"),
+            "is_active": request.path == url_for('admin.delivery_radius'),
         },
         {
             "icon": "fa-solid fa-gears me-2",
             "text": "Settings",
             "href": url_for("admin.settings"),
+            "is_active": request.path == url_for('admin.settings'),
         },
     ]
