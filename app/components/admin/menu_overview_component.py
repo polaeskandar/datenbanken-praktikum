aba7b5bf1321288000b70285555c3b0426fa2385
Pola Eskandar <pola.eskandar@codingstreamer.com>
2024-12-14 15:50:40 +0100

    Add the design page

---
+++ app/components/admin/menu_overview_component.py
@@ -0,0 +1,5 @@
+from flask import render_template
+
+
+def menu_overview_component():
+    return render_template("components/admin/menu_overview_component.html")
--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -5,11 +5,14 @@ from app.components.admin.edit_settings_component import edit_settings_component
 from app.components.admin.get_delivery_radius_component import (
     get_delivery_radius_component,
 )
+from app.components.admin.menu_overview_component import menu_overview_component
 from app.components.admin.menu_page_navigation_component import (
     menu_page_navigation_component,
 )
 from app.components.admin.orders_table_component import orders_table_component
-from app.components.admin.set_delivery_radius_component import set_delivery_radius_component
+from app.components.admin.set_delivery_radius_component import (
+    set_delivery_radius_component,
+)
 from app.components.admin.set_opening_hours_component import set_opening_hours_component
 from app.components.footer_component import footer_component
 from app.components.navbar_component import navbar_component
@@ -37,6 +40,47 @@ def index() -> Response:
     }
 
     return render_page("admin.html", "Admin", components)
+
+
+@admin_routes.route("/menu", methods=["GET", "POST"])
+def menu_overview() -> Response:
+    components = {
+        "header": [
+            navbar_component(),
+        ],
+        "aside": [
+            aside_menu_component(),
+        ],
+        "main": [
+            menu_page_navigation_component(),
+            menu_overview_component(),
+        ],
+        "footer": [
+            footer_component(),
+        ],
+    }
+
+    return render_page("admin.html", "Menu", components)
+
+
+@admin_routes.route("/menu/add", methods=["GET", "POST"])
+def add_menu_item() -> Response:
+    components = {
+        "header": [
+            navbar_component(),
+        ],
+        "aside": [
+            aside_menu_component(),
+        ],
+        "main": [
+            menu_page_navigation_component(),
+        ],
+        "footer": [
+            footer_component(),
+        ],
+    }
+
+    return render_page("admin.html", "Menu - Add Item", components)
 
 
 @admin_routes.route("/opening-hours", methods=["GET", "POST"])
